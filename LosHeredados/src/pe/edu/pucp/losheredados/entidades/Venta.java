0e5a64d226e5393d96229f2194163d50f4be9b33
AsNiTe <89858897+AsNiTe@users.noreply.github.com>
2022-04-06 20:55:55 -0500

    Update Venta.java

--- LosHeredados/src/pe/edu/pucp/losheredados/entidades/Venta.java
+++ LosHeredados/src/pe/edu/pucp/losheredados/entidades/Venta.java
@@ -1,36 +1,23 @@
 package pe.edu.pucp.losheredados.entidades;
 import java.util.Date;
 class Venta{
-
-    /**
-     * @return the tipoDePago
-     */
-    public String getTipoDePago() {
-        return tipoDePago;
-    }
-
-    /**
-     * @param tipoDePago the tipoDePago to set
-     */
-    public void setTipoDePago(String tipoDePago) {
-        this.tipoDePago = tipoDePago;
-    }
-
-    /**
-     * @return the fechaEntrega
-     */
-    public Date getFechaEntrega() {
-        return fechaEntrega;
-    }
-
-    /**
-     * @param fechaEntrega the fechaEntrega to set
-     */
-    public void setFechaEntrega(Date fechaEntrega) {
-        this.fechaEntrega = fechaEntrega;
-    }
 	private String tipoDePago;
 	private Date fechaEntrega;
+	
+	public String getTipoDePago() {
+		return tipoDePago;
+	}
+	public void setTipoDePago(String tipoDePago) {
+		this.tipoDePago = tipoDePago;
+	}
+
+	public Date getFechaEntrega() {
+		return fechaEntrega;
+	}
+	public void setFechaEntrega(Date fechaEntrega) {
+		this.fechaEntrega = fechaEntrega;
+	}
+
 	public Venta(String tipoDePago,Date fechaEntrega){
 		this.tipoDePago=tipoDePago;
 		this.fechaEntrega=fechaEntrega;
