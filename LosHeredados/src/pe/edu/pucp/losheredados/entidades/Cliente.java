e696d67dcad23cd276621daebbd2a22d5ea2f2c9
AsNiTe <89858897+AsNiTe@users.noreply.github.com>
2022-04-06 20:25:14 -0500

    Update Cliente.java

--- LosHeredados/src/pe/edu/pucp/losheredados/entidades/Cliente.java
+++ LosHeredados/src/pe/edu/pucp/losheredados/entidades/Cliente.java
@@ -1,66 +1,41 @@
 package pe.edu.pucp.losheredados.entidades;
 import java.util.Date;
+import java.util.ArrayList;
 class Cliente extends Usuario{
-
-    /**
-     * @return the razonSocial
-     */
-    public int getRazonSocial() {
-        return razonSocial;
-    }
-
-    /**
-     * @param razonSocial the razonSocial to set
-     */
-    public void setRazonSocial(int razonSocial) {
-        this.razonSocial = razonSocial;
-    }
-
-    /**
-     * @return the personaContacto
-     */
-    public String getPersonaContacto() {
-        return personaContacto;
-    }
-
-    /**
-     * @param personaContacto the personaContacto to set
-     */
-    public void setPersonaContacto(String personaContacto) {
-        this.personaContacto = personaContacto;
-    }
-
-    /**
-     * @return the categoria
-     */
-    public Cliente_Categoria getCategoria() {
-        return categoria;
-    }
-
-    /**
-     * @param categoria the categoria to set
-     */
-    public void setCategoria(Cliente_Categoria categoria) {
-        this.categoria = categoria;
-    }
-
-    /**
-     * @return the ultimoContacto
-     */
-    public Date getUltimoContacto() {
-        return ultimoContacto;
-    }
-
-    /**
-     * @param ultimoContacto the ultimoContacto to set
-     */
-    public void setUltimoContacto(Date ultimoContacto) {
-        this.ultimoContacto = ultimoContacto;
-    }
 	private int razonSocial;
 	private String personaContacto;
 	private Cliente_Categoria categoria;
 	private Date ultimoContacto;
+	private Arraylist<> Obra;
+
+	public int getRazonSocial() {
+	return razonSocial;
+	}
+	public void setRazonSocial(int razonSocial) {
+	this.razonSocial = razonSocial;
+	}
+
+	public String getPersonaContacto() {
+	return personaContacto;
+	}
+	public void setPersonaContacto(String personaContacto) {
+	this.personaContacto = personaContacto;
+	}
+
+	public Cliente_Categoria getCategoria() {
+	return categoria;
+	}
+	public void setCategoria(Cliente_Categoria categoria) {
+	this.categoria = categoria;
+	}
+
+	public Date getUltimoContacto() {
+	return ultimoContacto;
+	}
+
+	public void setUltimoContacto(Date ultimoContacto) {
+	this.ultimoContacto = ultimoContacto;
+	}
 	
 	public Cliente(String nombre,int telefono,String correo, int razonSocial,String personaContacto,Cliente_Categoria categoria, Date ultimoContacto){
 		super(nombre,telefono,correo);
