b3ef32e611023ac33b47e1020c00336d150fcc69
AsNiTe <89858897+AsNiTe@users.noreply.github.com>
2022-04-06 20:30:23 -0500

    Update Cliente_Categoria.java

--- LosHeredados/src/pe/edu/pucp/losheredados/entidades/Cliente_Categoria.java
+++ LosHeredados/src/pe/edu/pucp/losheredados/entidades/Cliente_Categoria.java
@@ -1,35 +1,22 @@
 package pe.edu.pucp.losheredados.entidades;
 class Cliente_Categoria{
-
-    /**
-     * @return the nombre
-     */
-    public String getNombre() {
-        return nombre;
-    }
-
-    /**
-     * @param nombre the nombre to set
-     */
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    /**
-     * @return the lineaCredito
-     */
-    public float getLineaCredito() {
-        return lineaCredito;
-    }
-
-    /**
-     * @param lineaCredito the lineaCredito to set
-     */
-    public void setLineaCredito(float lineaCredito) {
-        this.lineaCredito = lineaCredito;
-    }
 	private String nombre;
 	private float lineaCredito; 
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public float getLineaCredito() {
+		return lineaCredito;
+	}
+	public void setLineaCredito(float lineaCredito) {
+		this.lineaCredito = lineaCredito;
+	}
+	
 	public Cliente_Categoria(String nombre,float lineaCredito){
 		this.nombre=nombre;
 		this.lineaCredito=lineaCredito;
