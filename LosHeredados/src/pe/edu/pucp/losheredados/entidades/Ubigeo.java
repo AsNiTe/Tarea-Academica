313d2527981be7b659c27862f35aeae7221a590d
AsNiTe <89858897+AsNiTe@users.noreply.github.com>
2022-04-06 20:54:23 -0500

    Update Ubigeo.java

--- LosHeredados/src/pe/edu/pucp/losheredados/entidades/Ubigeo.java
+++ LosHeredados/src/pe/edu/pucp/losheredados/entidades/Ubigeo.java
@@ -1,69 +1,51 @@
 package pe.edu.pucp.losheredados.entidades;
 class Ubigeo{
-
-    /**
-     * @return the idUbigeo
-     */
-    public int getIdUbigeo() {
-        return idUbigeo;
-    }
-
-    /**
-     * @param idUbigeo the idUbigeo to set
-     */
-    public void setIdUbigeo(int idUbigeo) {
-        this.idUbigeo = idUbigeo;
-    }
-
-    /**
-     * @return the distrito
-     */
-    public String getDistrito() {
-        return distrito;
-    }
-
-    /**
-     * @param distrito the distrito to set
-     */
-    public void setDistrito(String distrito) {
-        this.distrito = distrito;
-    }
-
-    /**
-     * @return the provincia
-     */
-    public String getProvincia() {
-        return provincia;
-    }
-
-    /**
-     * @param provincia the provincia to set
-     */
-    public void setProvincia(String provincia) {
-        this.provincia = provincia;
-    }
-
-    /**
-     * @return the departamento
-     */
-    public String getDepartamento() {
-        return departamento;
-    }
-
-    /**
-     * @param departamento the departamento to set
-     */
-    public void setDepartamento(String departamento) {
-        this.departamento = departamento;
-    }
 	private int idUbigeo;
 	private String distrito;
 	private String provincia;
 	private String departamento;
-	public Ubigeo(int idUbigeo,String distrito,String provincia,String departamento){
+	private float costoDeEnvio;
+	
+	public int getIdUbigeo() {
+		return idUbigeo;
+	}
+	public void setIdUbigeo(int idUbigeo) {
+		this.idUbigeo = idUbigeo;
+	}
+
+	public String getDistrito() {
+		return distrito;
+	}
+	public void setDistrito(String distrito) {
+		this.distrito = distrito;
+	}
+
+	public String getProvincia() {
+		return provincia;
+	}
+	public void setProvincia(String provincia) {
+		this.provincia = provincia;
+	}
+
+	public String getDepartamento() {
+		return departamento;
+	}
+	public void setDepartamento(String departamento) {
+		this.departamento = departamento;
+	}
+	
+	public float getCostoDeEnvio() {
+		return costoDeEnvio;
+	}
+	public void setCostoDeEnvio(float costoDeEnvio) {
+		this.costoDeEnvio = costoDeEnvio;
+	}
+	
+	public Ubigeo(int idUbigeo,String distrito,String provincia,String departamento,float costoDeEnvio){
 		this.idUbigeo=idUbigeo;
 		this.distrito=distrito;
 		this.provincia=provincia;
 		this.departamento=departamento;
+		this.costoDeEnvio=costoDeEnvio;
 	}
 }
