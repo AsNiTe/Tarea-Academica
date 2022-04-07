e2dd9853d40bdf746b0368c8e18df44f6a500611
AsNiTe <89858897+AsNiTe@users.noreply.github.com>
2022-04-06 20:22:21 -0500

    Update Categoria.java

--- LosHeredados/src/pe/edu/pucp/losheredados/entidades/Categoria.java
+++ LosHeredados/src/pe/edu/pucp/losheredados/entidades/Categoria.java
@@ -1,36 +1,22 @@
 package pe.edu.pucp.losheredados.entidades;
 class Categoria{
-
-    /**
-     * @return the nombre
-     */
-    public String getNombre() {
-        return nombre;
-    }
-
-    /**
-     * @param nombre the nombre to set
-     */
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    /**
-     * @return the etapaRecomendada
-     */
-    public String getEtapaRecomendada() {
-        return etapaRecomendada;
-    }
-
-    /**
-     * @param etapaRecomendada the etapaRecomendada to set
-     */
-    public void setEtapaRecomendada(String etapaRecomendada) {
-        this.etapaRecomendada = etapaRecomendada;
-    }
 	private String nombre;
 	private String etapaRecomendada;
 	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getEtapaRecomendada() {
+		return etapaRecomendada;
+	}
+	public void setEtapaRecomendada(String etapaRecomendada) {
+		this.etapaRecomendada = etapaRecomendada;
+	}
+
 	public Categoria(String nombre,String etapaRecomendada){
 		this.nombre=nombre;
 		this.etapaRecomendada=etapaRecomendada;
