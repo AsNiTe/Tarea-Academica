ab21e4ed2cbc9c5a4856562c275432c0cbd1de4c
AsNiTe <89858897+AsNiTe@users.noreply.github.com>
2022-04-06 20:21:05 -0500

    Update Almacen.java

--- LosHeredados/src/pe/edu/pucp/losheredados/entidades/Almacen.java
+++ LosHeredados/src/pe/edu/pucp/losheredados/entidades/Almacen.java
@@ -1,29 +1,30 @@
 package pe.edu.pucp.losheredados.entidades;
 import java.util.ArrayList;
 class Almacen{
-    public ArrayList<Producto> getProducto() {
-        return producto;
-    }
-    public void setProducto(ArrayList<Producto> producto) {
-        this.producto = producto;
-    }
-
-    public String getDireccion() {
-        return direccion;
-    }
-    public void setDireccion(String direccion) {
-        this.direccion = direccion;
-    }
 	private ArrayList<Producto> producto;
 	private String direccion;
 
+	public ArrayList<Producto> getProducto() {
+	return producto;
+	}
+	public void setProducto(ArrayList<Producto> producto) {
+	this.producto = producto;
+	}
+
+	public String getDireccion() {
+	return direccion;
+	}
+	public void setDireccion(String direccion) {
+	this.direccion = direccion;
+	}
+	
 	public ArrayList<Producto> getProductos(){
-		return getProducto();
+	return getProducto();
 	}
 	public void setProductos(ArrayList<Producto> producto){
-            this.setProducto(producto);
+	this.setProducto(producto);
 	}
 	public Almacen(String direccion){
-		this.direccion=direccion;
+	this.direccion=direccion;
 	}
 }
