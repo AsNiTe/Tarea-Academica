d42282dcac410777f83fd54f76d2cbc29ca8e4d3
AsNiTe <89858897+AsNiTe@users.noreply.github.com>
2022-04-06 20:55:13 -0500

    Update Usuario.java

--- LosHeredados/src/pe/edu/pucp/losheredados/entidades/Usuario.java
+++ LosHeredados/src/pe/edu/pucp/losheredados/entidades/Usuario.java
@@ -1,50 +1,30 @@
 package pe.edu.pucp.losheredados.entidades;
 class Usuario{
-
-    /**
-     * @return the nombre
-     */
-    public String getNombre() {
-        return nombre;
-    }
-
-    /**
-     * @param nombre the nombre to set
-     */
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    /**
-     * @return the telefono
-     */
-    public int getTelefono() {
-        return telefono;
-    }
-
-    /**
-     * @param telefono the telefono to set
-     */
-    public void setTelefono(int telefono) {
-        this.telefono = telefono;
-    }
-
-    /**
-     * @return the correo
-     */
-    public String getCorreo() {
-        return correo;
-    }
-
-    /**
-     * @param correo the correo to set
-     */
-    public void setCorreo(String correo) {
-        this.correo = correo;
-    }
 	private String nombre;
 	private int telefono;
 	private String correo;
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(int telefono) {
+		this.telefono = telefono;
+	}
+
+	public String getCorreo() {
+		return correo;
+	}
+	public void setCorreo(String correo) {
+		this.correo = correo;
+	}
+	
 	public Usuario(String nombre,int telefono,String correo){
 		this.nombre=nombre;
 		this.telefono=telefono;
