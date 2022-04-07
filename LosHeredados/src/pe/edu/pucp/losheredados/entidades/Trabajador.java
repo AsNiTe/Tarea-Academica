6735451a19295d2558929177d1d44df9f4ef4233
AsNiTe <89858897+AsNiTe@users.noreply.github.com>
2022-04-06 20:51:32 -0500

    Update Trabajador.java

--- LosHeredados/src/pe/edu/pucp/losheredados/entidades/Trabajador.java
+++ LosHeredados/src/pe/edu/pucp/losheredados/entidades/Trabajador.java
@@ -1,35 +1,40 @@
 package pe.edu.pucp.losheredados.entidades;
+import java.out.ArrayList;
+
 class Trabajador extends Usuario{
-
-    /**
-     * @return the area
-     */
-    public String getArea() {
-        return area;
-    }
-
-    /**
-     * @param area the area to set
-     */
-    public void setArea(String area) {
-        this.area = area;
-    }
-
-    /**
-     * @return the cargo
-     */
-    public String getCargo() {
-        return cargo;
-    }
-
-    /**
-     * @param cargo the cargo to set
-     */
-    public void setCargo(String cargo) {
-        this.cargo = cargo;
-    }
 	private String area;
 	private String cargo;
+	private ArrayList<Servicio> servicio;
+	private ArrayList<Cotizacion> cotizacion;
+	
+	public String getArea() {
+	return area;
+	}
+	public void setArea(String area) {
+	this.area = area;
+	}
+
+	public String getCargo() {
+	return cargo;
+	}
+	public void setCargo(String cargo) {
+	this.cargo = cargo;
+	}
+	
+	public ArrayList<Servicio> getServicio() {
+		return servicio;
+	}
+	public void setServicio(ArrayList<Servicio> servicio) {
+		this.servicio = servicio;
+	}
+	
+	public ArrayList<Cotizacion> getCotizacion() {
+		return cotizacion;
+	}
+	public void setCotizacion(ArrayList<Cotizacion> cotizacion) {
+		this.cotizacion = cotizacion;
+	}
+	
 	public Trabajador(String nombre, int telefono,String correo,String area,String cargo){
 		super(nombre,telefono,correo);
 		this.area=area;
