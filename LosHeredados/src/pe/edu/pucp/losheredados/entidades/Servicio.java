b5a9463e03f9b02c9e4f7df774ae0e69246f92b5
AsNiTe <89858897+AsNiTe@users.noreply.github.com>
2022-04-06 20:47:59 -0500

    Update Servicio.java

--- LosHeredados/src/pe/edu/pucp/losheredados/entidades/Servicio.java
+++ LosHeredados/src/pe/edu/pucp/losheredados/entidades/Servicio.java
@@ -1,59 +1,33 @@
 package pe.edu.pucp.losheredados.entidades;
 import java.util.ArrayList;
 class Servicio{
-
-    /**
-     * @return the nombre
-     */
-    public String getNombre() {
-        return nombre;
-    }
-
-    /**
-     * @param nombre the nombre to set
-     */
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    /**
-     * @return the costo
-     */
-    public float getCosto() {
-        return costo;
-    }
-
-    /**
-     * @param costo the costo to set
-     */
-    public void setCosto(float costo) {
-        this.costo = costo;
-    }
-
-    /**
-     * @return the encargado
-     */
-    public ArrayList<Trabajador> getEncargado() {
-        return encargado;
-    }
-
-    /**
-     * @param encargado the encargado to set
-     */
-    public void setEncargado(ArrayList<Trabajador> encargado) {
-        this.encargado = encargado;
-    }
 	private String nombre;
 	private float costo;
 	private ArrayList<Trabajador> encargado;
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public float getCosto() {
+		return costo;
+	}
+	public void setCosto(float costo) {
+		this.costo = costo;
+	}
+
+	public ArrayList<Trabajador> getEncargado() {
+		return encargado;
+	}
+	public void setEncargado(ArrayList<Trabajador> encargado) {
+		this.encargado = encargado;
+	}
+
 	public Servicio(String nombre,float costo){
 		this.nombre=nombre;
 		this.costo=costo;
-	}
-	public ArrayList<Trabajador> getEncargados(){
-		return getEncargado();
-	}
-	public void setEncargados(ArrayList<Trabajador> encargado){
-            this.setEncargado(encargado);
 	}
 }
