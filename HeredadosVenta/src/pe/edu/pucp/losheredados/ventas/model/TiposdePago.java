5eed2017b4e3a605bb5bb8ed10ba68035fcf0e8e
Roberto Carlos <a20171016@pucp.edu.pe>
2022-04-26 22:39:00 -0500

    "error corregido pe.edu.pe.pucp en heredados ventas"

--- HeredadosVenta/src/pe/edu/pucp/losheredados/ventas/model/TiposdePago.java
+++ HeredadosVenta/src/pe/edu/pucp/losheredados/ventas/model/TiposdePago.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package pe.edu.pe.pucp.losheredados.ventas.model;
+package pe.edu.pucp.losheredados.ventas.model;
 
 /**
  *
